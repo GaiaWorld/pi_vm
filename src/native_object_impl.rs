23d5aa68a0b72c82d6c9d8054b5a18d4370c8ebc
zuon <dev@dev.com>
2018-06-28 10:41:13 +0800

    修复了编译bug

--- src/native_object_impl.rs
+++ src/native_object_impl.rs
@@ -18,59 +18,34 @@ pub extern "C" fn native_object_function_call(
         let vm = unsafe { js.get_vm() };
         unsafe { dukc_switch_context(vm); }
         let vec = args_to_vec(vm, args_size, args_type as *const u8, args as *const u32);
-        // match bon_call(js.clone(), hash, vec) {
-        //     Some(CallResult::Ok) => {
-        //         unsafe { dukc_switch_context(vm); }
-        //         Arc::into_raw(js);
-        //         return 1
-        //     },
-        //     Some(CallResult::Err(reason)) => {
-        //         unsafe {
-        //             dukc_switch_context(vm); //必须先切换上下文，再抛出异常
-        //             dukc_throw(vm, CString::new(reason).unwrap().as_ptr()); 
-        //         }
-        //         Arc::into_raw(js);
-        //         return 0;
-        //     }
-        //     None => {
-        //         //没有立即返回，则表示会阻塞，并异步返回
-        //         unsafe {
-        //             dukc_switch_context(vm);
-        //             Arc::into_raw(js);
-        //             if dukc_vm_status_switch(vm, JSStatus::SingleTask as i8, JSStatus::WaitBlock as i8) == JSStatus::SingleTask as i8 {
-        //                 //改变状态成功，防止虚拟机在当前同步任务完成后被立即回收，回收权利交由异步任务
-        //                 return 0;
-        //             } else {
-        //                 return -1;
-        //             }
-        //         }
-        //     },
-        // }
-        //测试同步返回和异步回调
-        // if hash < 0xffffffff {
-        //     js.new_u32(hash);
-        // } else {
-        //     js.new_str("Hello".to_string()).get_value() as *const c_void;
-        // }
-        // unsafe { dukc_switch_context(vm); }
-        // Arc::into_raw(js);
-        // return 1;
-        //测试同步阻塞返回和异步回调
-        if hash < 0xffffffff {
-            use pi_lib::atom::Atom;
-            use adapter::JSMsg;
-            let args = Box::new(move |tmp: Arc<JS>| {
-                tmp.new_u32(hash);
-                1
-            });
-            js.push(JSMsg::new(hash, args, Atom::from("callback by async call")));
+        match bon_call(js.clone(), hash, vec) {
+            Some(CallResult::Ok) => {
+                unsafe { dukc_switch_context(vm); }
+                Arc::into_raw(js);
+                return 1
+            },
+            Some(CallResult::Err(reason)) => {
+                unsafe {
+                    dukc_switch_context(vm); //必须先切换上下文，再抛出异常
+                    dukc_throw(vm, CString::new(reason).unwrap().as_ptr()); 
+                }
+                Arc::into_raw(js);
+                return 0;
+            }
+            None => {
+                //没有立即返回，则表示会阻塞，并异步返回
+                unsafe {
+                    dukc_switch_context(vm);
+                    Arc::into_raw(js);
+                    if dukc_vm_status_switch(vm, JSStatus::SingleTask as i8, JSStatus::WaitBlock as i8) == JSStatus::SingleTask as i8 {
+                        //改变状态成功，防止虚拟机在当前同步任务完成后被立即回收，回收权利交由异步任务
+                        return 0;
+                    } else {
+                        return -1;
+                    }
+                }
+            },
         }
-        unsafe {
-            dukc_switch_context(vm);
-            dukc_vm_status_switch(vm, JSStatus::SingleTask as i8, JSStatus::WaitBlock as i8);
-        }
-        Arc::into_raw(js);
-        return 0;
 }
 
 //转换参数
