e561562f3959d4d6609f4e5933b32cf0100e0549
zuon <dev@dev.com>
2018-06-28 16:22:03 +0800

    提供了ChannelMap

--- src/channel_map.rs
+++ src/channel_map.rs
@@ -1,21 +1,20 @@
-use std::any::Any;
 use std::sync::Arc;
-use std::boxed::FnBox;
 use std::clone::Clone;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 
 use pi_lib::atom::Atom;
-use pi_lib::handler::{Env, GenType, Handler};
+use pi_lib::handler::{Env, GenType, Handler, Args};
 
 use adapter::JS;
+use pi_vm_impl::push_callback;
 
 /*
 * 通道对端
 */
 pub enum VMChannelPeer {
-    Any,    //任意
-    VM(JS), //虚拟机
+    Any,            //任意虚拟机
+    VM(Arc<JS>),    //指定虚拟机
 }
 
 /*
@@ -37,10 +36,10 @@ impl Env for VMChannel {
 
     fn set_attr(&mut self, key: Atom, value: GenType) -> Option<GenType> {
         match self.attrs.entry(key) {
-            Entry::Occupied(mut e) => {
+            Entry::Occupied(ref mut e) => {
                 Some(e.insert(value))
             },
-            Entry::Vacant(mut e) => {
+            Entry::Vacant(e) => {
                 e.insert(value);
                 None
             },
@@ -64,12 +63,24 @@ impl VMChannel {
 
     //发送消息
     pub fn send(&self, _name: Atom, _msg: Arc<Vec<u8>>) {
-
+        //TODO
+        &self.dst;
     }
 
     //回应请求
-    pub fn response(&self, callback: u32, args: Box<FnBox(Arc<JS>) -> usize>) {
-
+    pub fn response(&self, callback: u32, result: Arc<Vec<u8>>) -> bool {
+        match self.src {
+            VMChannelPeer::VM(ref js) => {
+                let args = Box::new(move |vm: Arc<JS>| -> usize {
+                    let array = vm.new_uint8_array(result.len() as u32);
+					array.from_bytes(result.as_slice());
+                    1
+                });
+                push_callback(js.clone(), callback, args, Atom::from("vm async call response task"));
+                true
+            },
+            _ => false
+        }
     }
 }
 
@@ -77,8 +88,8 @@ impl VMChannel {
 * 虚拟机通道表
 */
 pub struct VMChannelMap {
-    gray: usize,                        //灰度值
-    map: HashMap<Atom, Arc<Handler<A = Arc<Vec<u8>>, B = (), C = (), D = (), E = (), F = (), G = (), H = (), HandleResult = ()>>>,   //通道表
+    gray: usize,                                                                                                                    //灰度值
+    map: HashMap<Atom, Arc<Handler<A = Arc<Vec<u8>>, B = u32, C = (), D = (), E = (), F = (), G = (), H = (), HandleResult = ()>>>, //通道表
 }
 
 impl VMChannelMap {
@@ -90,26 +101,55 @@ impl VMChannelMap {
         }
     }
 
-    //设置指定名称的通道，返回同名的上一个通道
-    pub fn set(&mut self, name: Atom, handler: Arc<Handler<A = Arc<Vec<u8>>, B = (), C = (), D = (), E = (), F = (), G = (), H = (), HandleResult = ()>>) -> Option<Arc<Handler<A = Arc<Vec<u8>>, B = (), C = (), D = (), E = (), F = (), G = (), H = (), HandleResult = ()>>> {
+    //获取灰度值
+    pub fn get_gray(&self) -> usize {
+        self.gray
+    }
+
+    //设置灰度值
+    pub fn set_gray(&mut self, gray: usize) -> usize {
+        let old = self.gray;
+        self.gray = gray;
+        old
+    }
+
+    //获取处理器数量
+    pub fn size(&self) -> usize {
+        self.map.len()
+    }
+
+    //设置指定名称的处理器，返回同名的上一个处理器
+    pub fn set(&mut self, name: Atom, handler: Arc<Handler<A = Arc<Vec<u8>>, B = u32, C = (), D = (), E = (), F = (), G = (), H = (), HandleResult = ()>>) -> Option<Arc<Handler<A = Arc<Vec<u8>>, B = u32, C = (), D = (), E = (), F = (), G = (), H = (), HandleResult = ()>>> {
         match self.map.entry(name) {
-            Entry::Occupied(mut e) => {
+            Entry::Occupied(ref mut e) => {
                 Some(e.insert(handler))
             },
-            Entry::Vacant(mut e) => {
+            Entry::Vacant(e) => {
                 e.insert(handler);
                 None
             },
         }
     }
 
-    //移除指定名称的通道，返回通道
-    pub fn remove(&mut self, name: Atom) -> Option<Arc<Handler<A = Arc<Vec<u8>>, B = (), C = (), D = (), E = (), F = (), G = (), H = (), HandleResult = ()>>> {
+    //移除指定名称的处理器，返回处理器
+    pub fn remove(&mut self, name: Atom) -> Option<Arc<Handler<A = Arc<Vec<u8>>, B = u32, C = (), D = (), E = (), F = (), G = (), H = (), HandleResult = ()>>> {
         self.map.remove(&name)
     }
 
     //请求
-    pub fn request(&self, name: Atom, msg: Arc<Vec<u8>>, callback: u32) {
+    pub fn request(&self, js: Arc<JS>, name: Atom, msg: Arc<Vec<u8>>, callback: u32) -> bool {
+        let handler = match self.map.get(&name) {
+            None => {
+                return false;
+            },
+            Some(h) => {
+                h
+            },
+        };
 
+        let mut channel = VMChannel::new(VMChannelPeer::VM(js), VMChannelPeer::Any);
+        channel.set_attr(Atom::from("_$gray"), GenType::USize(self.gray));
+        handler.handle(Arc::new(channel), name, Args::TwoArgs(msg, callback));
+        true
     }
 }
